eac9de115f2d960fcbf607560de5e77e521c2bbb
xuxiaofan <xuxiaofan@test.com>
2025-12-28 08:44:44 +0800

    perf:优化404倒计时后返回首页、未知路由返回首页

--- web/src/views/exception/404.vue
+++ web/src/views/exception/404.vue
@@ -1,22 +1,45 @@
+<script lang="ts" setup>
+  import { ref, onMounted, onBeforeUnmount } from 'vue';
+  import { useRouter, onBeforeRouteLeave } from 'vue-router';
+
+  const router = useRouter();
+  const countdown = ref(5);
+  let intervalId: ReturnType<typeof setInterval>;
+
+  function goHome() {
+    router.push('/');
+  }
+
+  const startCountdown = () => {
+    intervalId = setInterval(() => {
+      if (countdown.value > 0) {
+        countdown.value--;
+      } else {
+        router.push('/');
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  };
+
+  onMounted(startCountdown);
+  onBeforeUnmount(() => clearInterval(intervalId));
+  onBeforeRouteLeave(() => clearInterval(intervalId));
+</script>
+
 <template>
   <div class="flex flex-col justify-center page-container">
     <div class="text-center">
       <img src="~@/assets/images/exception/404.svg" alt="" />
     </div>
     <div class="text-center">
-      <h1 class="text-base text-gray-500">抱歉，你访问的页面不存在</h1>
+      <h1 class="text-base text-gray-500">
+        抱歉，页面不存在！<br />
+        {{ countdown }}秒后自动返回首页
+      </h1>
       <n-button type="info" @click="goHome">回到首页</n-button>
     </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-  import { useRouter } from 'vue-router';
-  const router = useRouter();
-  function goHome() {
-    router.push('/');
-  }
-</script>
 
 <style lang="less" scoped>
   .page-container {
